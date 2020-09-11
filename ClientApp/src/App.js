202b49d1fd4358061dc833b208d5879f5a2d22c3
andyshen2 <andyshen96@gmail.com>
2020-09-11 12:33:24 +1200

    cleaning up routing page

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,69 +1,57 @@
-import React, { Component } from 'react';
-import { Router, Route, Redirect, Switch } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
-import  Login  from './components/Login';
-import Register  from './components/Register'
-import ToDo from './components/ToDo';
-import Protected from './ProtectedRoutes';
-import './custom.css'
-import { checkAuth } from './actions';
+import React, { Component } from "react";
+import { Router, Route, Redirect, Switch } from "react-router";
+import { Layout } from "./components/Layout";
+import { Home } from "./components/Home";
+import { FetchData } from "./components/FetchData";
+import { Counter } from "./components/Counter";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import ToDo from "./components/ToDo";
+import Protected from "./ProtectedRoutes";
+import "./custom.css";
+import { checkAuth } from "./actions";
 import { connect } from "react-redux";
 import { withRouter, BrowserRouter } from "react-router-dom";
 
-
 class App extends Component {
   static displayName = App.name;
-  // componentDidMount() {
-
-  //   console.log(this.props.checkAuth())
-    
-  // }
-  render () {
-    // console.log("state" ,this.props.state)
-    // const { auth } = this.props;
-    // so checks against isAuthenticated can pass
-    // this.props.checkAuth().then({
-      
-    // })
+  render() {
     return (
       <BrowserRouter>
-      <Layout>
-         
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/counter' component={Counter} />
-          <Route path='/fetch-data' component={FetchData} />
-          {/* <Route path='/to-do' component={props => <ToDo {...props} />} /> */}
-          
-          <Route path="/register" component={props => <Register {...props} />} />
-          <Route  exact path='/to-do' component={Protected(props => <ToDo {...props} />)} />
-          <Route exact path="/login" component={props => <Login {...props} />}  />
-
-        </Switch>
-        
-      </Layout>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/counter" component={Counter} />
+            <Route path="/fetch-data" component={FetchData} />
+            <Route
+              path="/register"
+              component={(props) => <Register {...props} />}
+            />
+            <Route
+              exact
+              path="/to-do"
+              component={Protected((props) => (
+                <ToDo {...props} />
+              ))}
+            />
+            <Route
+              exact
+              path="/login"
+              component={(props) => <Login {...props} />}
+            />
+          </Switch>
+        </Layout>
       </BrowserRouter>
     );
-    
   }
-
 }
-const mapDispatchToProps = dispatch => ({
-    
+const mapDispatchToProps = (dispatch) => ({
   checkAuth: () => dispatch(checkAuth()),
+});
 
-})
-
-const mapStateToProps = function(state) {
-  // console.log("state", state)
+const mapStateToProps = function (state) {
   return {
-
-    state: state.user
-  }
-}
-
-
+    state: state.user,
+  };
+};
 export default connect(mapStateToProps, mapDispatchToProps)(withRouter(App));
